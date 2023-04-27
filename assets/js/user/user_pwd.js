83a732e5f470fb101615977d87f97f55f74933ed
zengfancheng001 <863513651@qq.com>
2023-04-27 16:04:09 +0800

    完成个人中心开发

--- assets/js/user/user_pwd.js
+++ assets/js/user/user_pwd.js
@@ -1,34 +1,36 @@
-$(function() {
-  var form = layui.form
+$(function(){
+let form =layui.form
+form.verify({
+  pwd:[/^[\S]{6,12}$/, '密码必须为6到12位的非空字符'] ,
+  samePwd:function(value){
+    if(value === $('[name=oldPwd]').val()){
+      return '新旧密码不能相同！'
+    }
+  },
+  rePwd:function(value){
+    if(value!==$('[name=newPwd]').val()){
+      return '两次密码输入不一致！'
+    }
+  }
 
-  form.verify({
-    pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
-    samePwd: function(value) {
-      if (value === $('[name=oldPwd]').val()) {
-        return '新旧密码不能相同！'
+})
+
+$('.layui-form').on('submit',function(e){
+  e.preventDefault()
+  $.ajax({
+    method:'POST',
+    url:'/my/updatepwd',
+    data:$(this).serialize(),
+    success:function(res){
+      if(res.status!==0){
+        return layui.layer.msg('更新密码失败！')
       }
-    },
-    rePwd: function(value) {
-      if (value !== $('[name=newPwd]').val()) {
-        return '两次密码不一致！'
-      }
+      layui.layer.msg('更新密码成功！')
+      // 重置表单，jquery没有这个方法
+      $('.layui-form')[0].reset()
+
     }
   })
-
-  $('.layui-form').on('submit', function(e) {
-    e.preventDefault()
-    $.ajax({
-      method: 'POST',
-      url: '/my/updatepwd',
-      data: $(this).serialize(),
-      success: function(res) {
-        if (res.status !== 0) {
-          return layui.layer.msg('更新密码失败！')
-        }
-        layui.layer.msg('更新密码成功！')
-        // 重置表单
-        $('.layui-form')[0].reset()
-      }
-    })
-  })
+  
+})
 })
